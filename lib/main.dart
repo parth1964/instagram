9c1859249d09561c7c7f29d57ae478c01a0c4f10
ramanujparth <ramanujparth20@gmail.com>
2022-06-20 18:39:00 +0530

    Insta_Complete

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:instagram/providers/user_provider.dart';
 import 'package:instagram/responsive/mobile_screen_layout.dart';
 import 'package:instagram/responsive/responsive_layout.dart';
 import 'package:instagram/responsive/web_screen_layout.dart';
+
 import 'package:instagram/screens/login_screen.dart';
 import 'package:instagram/screens/signup_screen.dart';
 import 'package:instagram/utils/colors.dart';
@@ -14,14 +15,15 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // This Configuration is For Web
   if (kIsWeb) {
     await Firebase.initializeApp(
         options: const FirebaseOptions(
-      apiKey: "AIzaSyCjA_xzeXyTWj60rTx1shL4E932DnJkPZ4",
-      appId: "1:157402278780:web:79deae479dad46198dd2f9",
-      messagingSenderId: "157402278780",
-      projectId: "instagram-ac257",
-      storageBucket: "instagram-ac257.appspot.com",
+      apiKey: "Insert API Key Here",
+      appId: "Insert AppId ",
+      messagingSenderId: "Insert Messenger Sender Id",
+      projectId: "Insert Project Id",
+      storageBucket: "Insert Storage Bucket",
     ));
   } else {
     await Firebase.initializeApp();
